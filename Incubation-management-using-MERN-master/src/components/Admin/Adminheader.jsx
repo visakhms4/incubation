70154f4ec00da4848864e4a31c8f58ea918fa0c7
visakhms4 <visakhms4@gmail.com>
2023-02-21 10:41:11 +0530

    first commit

---
+++ Incubation-management-using-MERN-master/src/components/Admin/Adminheader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Admin/adminheader.css"
+function Adminheader() {
+  return (
+    <div className='header'>
+      <h1>Incubation</h1>
+    </div>
+  )
+}
+
+export default Adminheader
